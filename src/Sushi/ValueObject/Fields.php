165bd3cf50be6cb69fc9f83b15b1ce8ebb9b6430
Łukasz Bacik <mail@luka.sh>
2019-05-04 11:54:22 +0200

    removed unused code

--- src/Sushi/ValueObject/Fields.php
+++ src/Sushi/ValueObject/Fields.php
@@ -28,14 +28,6 @@ class Fields extends ArrayAccess
         return $this->keys;
     }
 
-    public function getKeysWithDefinitions(): array
-    {
-        if ($this->isAssociative($this->keys)) {
-            return $this->keys;
-        }
-        return [];
-    }
-
     private function isAssociative(array $arr): bool
     {
         foreach ($arr as $key => $value) {
